7497a1d141f5381dedfe7d98e99bcda2aebc6064
Brandon Kuczenski <brandon.kuczenski@301south.net>
2016-06-01 02:10:41 -0700

    create nodes in neo4j

---
+++ lcatools/providers/flow_quantity_graph_db.py
@@ -0,0 +1,28 @@
+column_mapping = {
+    'method': ('Tag', 'Quantity'),
+    'category': ('Tag', 'Quantity'),
+    'indicator': ('Tag', 'Quantity'),
+    'name': ('Property', 'Flowable'),
+    'compartment': ('Compartment', 'Flow'),
+    'subcompartment': ('Compartment', 'Flow'),
+    'CF 3.01': ('Tag', 'Factor'),
+    'CF 3.1': ('Tag', 'Factor'),
+    'Known issue': ('Tag', 'Factor')
+}
+
+from lcatools.interfaces import FlowQuantityInterface
+from py2neo import Graph, Schema, Node, Relationship
+
+
+class FlowQuantityGraphDb(FlowQuantityInterface):
+    """
+    A F-Q interface using a neo4j backend for graph-mazement
+    """
+    def __init__(self, *args, **kwargs):
+        """
+
+        :param args:
+        :param kwargs:
+        """
+        graph = Graph(*args, **kwargs)
+        super(FlowQuantityGraphDb, self).__init__(graph)

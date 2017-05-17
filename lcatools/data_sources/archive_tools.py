f9b2543730d4c523d315facedc6aab53c575938d
Brandon Kuczenski <brandon.kuczenski@301south.net>
2017-05-16 23:27:01 -0700

    Storehouse for data-source-specific manipulations (uslci needswork)

---
+++ lcatools/data_sources/archive_tools.py
@@ -0,0 +1,106 @@
+from collections import namedtuple
+from lcatools.entities import MissingAllocation
+from lcatools.exchanges import Exchange
+
+'''
+A ConfigFlowCharacterization provides a procedural mechanism for specifying flow quantity characterizations after
+loading an archive.  The 'flow_ref' and 'quantity_ref' have to lookup successfully in the archive.  Note- provisioning
+the archive itself is ad hoc. Don't have a structural way to do that yet, but someday it could be done with linked
+data (maybe?)
+'''
+ConfigFlowCharacterization = namedtuple("ConfigFlowCharacterization", ('flow_ref', 'quantity_ref', 'value'))
+
+'''
+A ConfigAllocation provides a procedural mechanism for specifying quantity-wise allocations of processes at load time.
+All that is required is a quantity; the process knows how to perform the allocation.  Note that reference flows not
+characterized with respect to the quantity will receive zero allocation.  So apply_flow_config first.
+'''
+ConfigAllocation = namedtuple("ConfigAllocation", ('process_ref', 'quantity_ref'))
+
+'''
+A ConfigBadReference provides a procedural mechanism for removing automatically-tagged reference flows, or for marking
+a byproduct as non-reference or non-allocatable.  The parts are 'process_ref', 'flow_ref', and 'direction', but if
+process_ref is None, then all instances of the flow_ref and direction will be marked non-reference.'''
+ConfigBadReference = namedtuple("ConfigBadReference", ('process_ref', 'flow_ref', 'direction'))
+
+
+def apply_flow_config(archive, flow_characterizations, overwrite=False):
+    """
+    Applies a list of ConfigFlowCharacterizations to an archive.
+    :param archive:
+    :param flow_characterizations:
+    :param overwrite: [False] overwrite if characterization is present
+    :return: nothing- the changes are made in-place
+    """
+    for cfc in flow_characterizations:
+        if not isinstance(cfc, ConfigFlowCharacterization):
+            raise TypeError('Entry is not a ConfigFlowCharacterization\n%s' % cfc)
+        flow = archive[cfc.flow_ref]
+        qty = archive[cfc.quantity_ref]
+        if flow.has_characterization(qty):
+            if overwrite:
+                flow.del_characterization(qty)
+            else:
+                print('Flow %s already characterized for %s. Skipping.' % (flow, qty))
+                pass
+        flow.add_characterization(qty, value=cfc.value)
+
+
+def apply_allocation(archive, allocations, overwrite=False):
+    """
+    Applies a list of ConfigAllocations to an archive.
+
+    If overwrite is True, the process's allocations are first removed.
+
+    If overwrite is False, the process is tested for allocation under each of its reference flows- if any are
+    already allocated, the allocation is aborted for the process.
+
+    :param archive:
+    :param allocations:
+    :param overwrite: [False] whether to strike and re-apply allocations if they already exist.
+    :return:
+    """
+    for al in allocations:
+        if not isinstance(al, ConfigAllocation):
+            raise TypeError('Entry is not a ConfigAllocation\n%s' % al)
+        p = archive[al.process_ref]
+        qty = archive[al.quantity_ref]
+        is_alloc = False
+        if overwrite:
+            for rf in p.reference_entity:
+                p.remove_allocation(rf)
+        else:
+            for rf in p.reference_entity:
+                try:
+                    is_alloc |= p.is_allocated(rf)
+                except MissingAllocation:
+                    is_alloc = True
+                    break
+
+        # now apply the allocation
+        if is_alloc:
+            print('Allocation already detected for %s. Skipping this configuration.' % p)
+            continue
+        else:
+            p.allocate_by_quantity(qty)
+
+
+def remove_bad_references(archive, bad_references):
+    """
+
+    :param archive:
+    :param bad_references: (process_ref, flow_ref, direction) - simply unset the reference exchange on the process
+      (None, flow_ref, direction) - unset the reference exchange on all processes in which it appears.
+    :return:
+    """
+    for br in bad_references:
+        if not isinstance(br, ConfigBadReference):
+            raise TypeError('Entry is not a ConfigBadReference\n%s' % br)
+        fl = archive[br.flow_ref]
+        if br.process_ref is None:
+            for x in archive.exchanges(fl, direction=br.direction):
+                if x.is_reference:
+                    x.process.remove_reference(x)
+        else:
+            pr = archive[br.process_ref]
+            pr.remove_reference(Exchange(pr, fl, br.direction))
---
+++ lcatools/data_sources/calrecycle_lca.py
@@ -0,0 +1,242 @@
+"""
+CalRecycle LCA
+Fragment Importer
+
+The CalRecycle fragment model is stored in four files:
+
+ Fragment.csv - (F) names and uuids for 55 fragments
+ FragmentFlow.csv - (FF) data for 888 fragment flows
+ FragmentNodeProcess.csv - (FNP) data for 247 nodes terminated to foreground processes
+ FragmentNodeFragment.csv - (FNF) data for 76 nodes terminated to sub-fragments
+
+The process data all come from 5 different ILCD archives:
+Full UO LCA Flat Export BK
+Full UO LCA Flat Export Improper
+Full UO LCA Flat Export Ecoinvent
+Full UO LCA Flat Export PE-SP22
+Full UO LCA Flat Export PE-SP24
+
+FNP references unique processes by uuid + version, but the version distinction is only relevant for differentiating
+PE-SP22 and PE-SP24.  In the future those will be distinguished by semantic references and not UUIDs.  For the present,
+the version differences can be ignored as they can be corrected later on.
+
+The objective here is to construct the 55 fragments and replicate the LCIA results.  Well, first to construct the
+fragments. And then to tune the qdb + catalog interfaces until the LCIA results correspond.
+
+What do we need to do for that?
+
+"""
+import os
+import csv
+from collections import namedtuple
+
+from lcatools.catalog.lc_resource import LcResource
+from lcatools.catalog.catalog import LcCatalog
+from lcatools.entities.editor import FragmentEditor
+from lcatools.exchanges import comp_dir
+from lcatools.terminations import FlowTermination
+
+
+SemanticRoot = namedtuple('SemanticRoot', ('path', 'root', 'privacy', 'priority'))
+
+
+DATA_ROOT = '/data/GitHub/CalRecycle/LCA_Data/'
+
+data_main = SemanticRoot(os.path.join(DATA_ROOT, 'Full UO LCA Flat Export BK'), 'calrecycle.uolca.core', 0, 10)
+data_improper = SemanticRoot(os.path.join(DATA_ROOT, 'Full UO LCA Flat Export Improper'), 'calrecycle.uolca.improper',
+                             0, 20)
+data_ecoinvent = SemanticRoot(os.path.join(DATA_ROOT, 'Full UO LCA Flat Export Ecoinvent'),
+                              'calrecycle.uolca.ecoinvent.2.2', 1, 30)
+data_pe24 = SemanticRoot(os.path.join(DATA_ROOT, 'Full UO LCA Flat Export PE-SP24'), 'calrecycle.uolca.pe.sp24', 1, 40)
+data_pe22 = SemanticRoot(os.path.join(DATA_ROOT, 'Full UO LCA Flat Export PE-SP22'), 'calrecycle.uolca.pe.sp22', 1, 50)
+
+
+def create_catalog(path):
+    return LcCatalog(path)
+
+
+def install_resources(cat):
+    for a in (data_main, data_improper, data_ecoinvent, data_pe22, data_pe24):
+        res = LcResource(a.root, a.path, 'IlcdArchive', interfaces='foreground',
+                         privacy=a.privacy, priority=a.priority, static=False)
+        cat.add_resource(res)
+
+
+def direction_map(did):
+    return {'1': 'Input',
+            1: 'Input',
+            '2': 'Output',
+            2: 'Output'}[did]
+
+
+def read_csv(file):
+    """
+    Creates a dict array where the keys are given in the first row of the CSV file
+    :param file:
+    :return: list of dicts.
+    """
+    with open(file, 'r') as fp:
+        reader = csv.reader(fp, delimiter=',', quotechar='"')
+        headers = next(reader)
+        d = []
+        for row in reader:
+            d.append(dict((headers[i], v) for i, v in enumerate(row)))
+    return d
+
+
+def dict_from_csv(file):
+    """
+    Creates a dict of string first-column -> second column.
+    :param file:
+    :return:
+    """
+    with open(file, 'r') as fp:
+        reader = csv.reader(fp, delimiter=',', quotechar='"')
+        headers = next(reader)
+        if len(headers) > 2:
+            raise ValueError('This function only works with 2-column tables')
+        d = dict()
+        for row in reader:
+            d[row[0]] = row[1]
+        return d
+
+
+class CalRecycleImporter(object):
+    """
+    Maybe we want a superclass; for now let's just put in what we need
+    """
+    @property
+    def fragment_dir(self):
+        return os.path.join(self._root, 'fragments')
+
+    def __init__(self, data_root):
+        self._root = data_root
+        self._stages = dict_from_csv(os.path.join(self.fragment_dir, 'FragmentStage.csv'))
+        self._f = read_csv(os.path.join(self.fragment_dir, 'Fragment.csv'))
+        self._ff = read_csv(os.path.join(self.fragment_dir, 'FragmentFlow.csv'))
+        self._fnp = read_csv(os.path.join(self.fragment_dir, 'FragmentNodeProcess.csv'))
+        self._fnf = read_csv(os.path.join(self.fragment_dir, 'FragmentNodeFragment.csv'))
+        self._bg = read_csv(os.path.join(self.fragment_dir, 'Background.csv'))
+
+        for term in self.fnp:
+            ffid = int(term['FragmentFlowID']) - 1
+            if 'Process' in self.ff[ffid]:
+                raise ValueError('Fragment %d is multiply terminated' % term['FragmentFlowID'])
+            self.ff[ffid]['Process'] = term
+        for term in self.fnf:
+            ffid = int(term['FragmentFlowID']) - 1
+            if 'SubFragment' in self.ff[ffid]:
+                raise ValueError('Fragment %d is multiply terminated' % term['FragmentFlowID'])
+            self.ff[ffid]['SubFragment'] = term
+        for frag in self.f:
+            ffid = int(frag['ReferenceFragmentFlowID']) - 1
+            if 'Fragment' in self.ff[ffid]:
+                raise ValueError('Fragment %d is multiply referenced' % frag['ReferenceFragmentFlowID'])
+            self.ff[ffid]['Fragment'] = frag
+
+        self._ed = FragmentEditor(interactive=False)
+        self._frags = dict()  # indexed with FragmentFlowID.str
+        self._fragments = dict()  # indexed with FragmentID.str
+
+    def stage(self, index):
+        try:
+            return self._stages[str(index)]
+        except KeyError:
+            return ''
+
+    @property
+    def f(self):
+        return self._f
+
+    @property
+    def ff(self):
+        return self._ff
+
+    @property
+    def fnp(self):
+        return self._fnp
+
+    @property
+    def fnf(self):
+        return self._fnf
+
+    def fragment_by_index(self, index):
+        return self._frags[int(index) - 1]
+
+    def fragment_flow_by_index(self, index):
+        return self.ff[int(index) - 1]
+
+    def fragment_from_fragment_flow(self, qi, ff):
+        """
+        :param qi: query interface for entity lookup
+        :param ff: should be a decorated entry from self._ff
+        :return:
+        """
+        if ff['FragmentFlowID'] in self._frags:
+            print('Already loaded: %s' % ff['FragmentFlowID'])
+            return
+        flow = qi.get(ff['FlowUUID'])
+        direction = direction_map(ff['DirectionID'])
+        stage = self.stage(ff['FragmentStageID'])
+        name = ff['Name']
+
+        # create the fragment, with or without a parent
+        if ff['ParentFragmentFlowID'] == '':
+            # must be a reference flow
+            frag_uuid = ff['Fragment']['FragmentUUID']
+            frag = self._ed.create_fragment(flow, comp_dir(direction), uuid=frag_uuid, StageName=stage, Name=name)
+            self._fragments[ff['Fragment']['FragmentID']] = frag
+        else:
+            try:
+                parent = self._frags[ff['ParentFragmentFlowID']]
+            except KeyError:
+                print('Recursing to %s' % ff['ParentFragmentFlowID'])
+                self.fragment_from_fragment_flow(qi, self.fragment_flow_by_index(ff['ParentFragmentFlowID']))
+                parent = self._frags[ff['ParentFragmentFlowID']]
+
+            frag = self._ed.create_fragment(flow, direction, Name=name, StageName=stage, parent=parent)
+
+        # save the fragment
+        if frag is None:
+            raise TypeError
+        self._frags[ff['FragmentFlowID']] = frag
+        print('adding %s... length is %d' % (ff['FragmentFlowID'], len(self._frags)))
+
+    def terminate_fragments(self, qi):
+        for ff in self.ff:
+            frag = self._frags[ff['FragmentFlowID']]
+            flow = frag.flow
+            # terminate the fragment
+            if ff['NodeTypeID'] == '1':
+                term_node = qi.get(ff['Process']['ProcessUUID'])
+                term_flow = qi.get(ff['Process']['FlowUUID'])
+                term = FlowTermination(frag, term_node, term_flow=term_flow)
+                frag.clear_termination()
+                frag.terminate(term)
+            elif ff['NodeTypeID'] == '2':
+                term_node = self._frags[ff['SubFragment']['SubFragmentID']]
+                term_flow = qi.get(ff['SubFragment']['FlowUUID'])
+                term = FlowTermination(frag, term_node, term_flow=term_flow)
+                frag.clear_termination()
+                frag.terminate(term)
+            elif ff['NodeTypeID'] == '4':
+                bg = next(row for row in self._bg
+                          if row['FlowUUID'] == flow.uuid and row['DirectionID'] == ff['DirectionID'])
+                if bg['TargetUUID'] != '':
+                    if bg['NodeTypeID'] == '1':
+                        term_node = qi.get(bg['TargetUUID'])
+                    else:
+                        term_node = next(_f for _f in self._fragments.values() if _f.uuid == bg['TargetUUID'])
+                    term = FlowTermination(frag, term_node, term_flow=flow)
+                    frag.clear_termination()
+                    frag.terminate(term)
+
+    def set_balances(self):
+        for f in self.ff:
+            if f['NodeTypeID'] == 1:
+                if f['Process']['ConservationFragmentFlowID'] != '':
+                    self._frags[f['Process']['ConservationFragmentFlowID']].set_balance_flow()
+
+
+if __name__ == '__main__':
+    print('foo')

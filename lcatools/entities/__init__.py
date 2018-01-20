d93e3828ff587f79b9a83a9688c174fc1818701e
Brandon Kuczenski <brandon.kuczenski@301south.net>
2018-01-19 17:12:00 -0800

    Move Basic Archive to entities pkg

--- lcatools/entities/__init__.py
+++ lcatools/entities/__init__.py
@@ -3,3 +3,4 @@ from .flows import LcFlow
 from .processes import LcProcess, NoReferenceFound, DuplicateExchangeError, MissingAllocation
 from .entities import LcEntity, entity_types
 from .fragments import LcFragment
+from .basic_archive import BasicArchive

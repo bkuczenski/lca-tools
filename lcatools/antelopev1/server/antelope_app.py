7277031e23152d7698a36ca6acfdb6767f55aa45
Brandon Kuczenski <brandon.kuczenski@301south.net>
2018-03-23 18:00:23 -0700

    Flask is hard

--- lcatools/antelopev1/server/antelope_app.py
+++ lcatools/antelopev1/server/antelope_app.py
@@ -1,8 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask
+from flask_marshmallow import Marshmallow
+from lcatools import LcCatalog
 
 
 app = Flask(__name__)
+app.config.
+app.catalog = LcCatalog(app.config['catalog_rootdir'])
 
 
-@app.route('/')
-def antelope_v1_api_help():
+ma = Marshmallow(app)
+
+
+def register_new_foreground(fname, study_pub):
+    app.register_blueprint()
